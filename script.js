7f75091acbd551dc692fc6913a16007159a767e0
MariaaClara15 <maria.silva2007@escola.pr.gov.br>
2026-05-25 12:10:58 -0300

    oiiiii

---
+++ script.js
@@ -0,0 +1,70 @@
+let pontos = 0;
+
+// ATUALIZA PONTUAÇÃO
+function atualizarPontuacao() {
+  document.getElementById("pontuacao").innerText =
+    pontos + " pontos";
+}
+
+// QUIZ
+function respostaCorreta() {
+  pontos += 10;
+
+  atualizarPontuacao();
+
+  document.getElementById("resultadoQuiz").innerText =
+    "✅ Resposta correta!";
+}
+
+function respostaErrada() {
+  document.getElementById("resultadoQuiz").innerText =
+    "❌ Resposta errada!";
+}
+
+// CALCULADORA DE ÁGUA
+function calcularAgua() {
+  let banho = document.getElementById("banho").value;
+
+  let litros = banho * 9;
+
+  document.getElementById("resultadoAgua").innerText =
+    "Você gasta aproximadamente " +
+    litros +
+    " litros por banho.";
+}
+
+// DICAS
+const dicas = [
+  "Desligue a torneira ao escovar os dentes.",
+  "Use sacolas reutilizáveis.",
+  "Apague as luzes ao sair.",
+  "Recicle o lixo corretamente.",
+  "Economize água no banho."
+];
+
+function gerarDica() {
+  let numero = Math.floor(Math.random() * dicas.length);
+
+  document.getElementById("dica").innerText =
+    dicas[numero];
+}
+
+// DESAFIOS
+const desafios = [
+  "Fique 1 dia sem copos descartáveis.",
+  "Plante uma árvore.",
+  "Economize água hoje.",
+  "Separe o lixo reciclável.",
+  "Ande de bicicleta."
+];
+
+function novoDesafio() {
+  let numero = Math.floor(Math.random() * desafios.length);
+
+  document.getElementById("desafio").innerText =
+    desafios[numero];
+
+  pontos += 5;
+
+  atualizarPontuacao();
+}
